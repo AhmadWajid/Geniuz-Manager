28c723824b66604b67ed5038ee46010d51d9c51b
Geniuz <73511795+AhmadWajid@users.noreply.github.com>
2022-03-24 15:39:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -69,5 +69,5 @@ async def process_queue():
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         bot.load_extension(f"cogs.{filename[:-3]}")
-bot.run("ODMzNTI2MzcyMDA1NDQ1NzAy.YHzn7Q.JOVuuDTTRvZEJOu39DkMhMB6Ds0")
+bot.run("testq23")
 # Insert your bot token here
